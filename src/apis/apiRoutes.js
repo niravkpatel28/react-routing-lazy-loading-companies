aaa790c7641e32639e6f296449f93c02e882f3f6
Nirav Patel <niravkpatel28@gmail.com>
2020-05-13 23:02:57 +0530

    HTTPS request url

--- src/apis/apiRoutes.js
+++ src/apis/apiRoutes.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://niravkpatel28.github.io/json-data-server";
+const baseUrl = "https://niravkpatel28.github.io/json-data-server";
 export const employees = `${baseUrl}/employees/employees.json`;
 export const companies = `${baseUrl}/companies/companies.json`;
