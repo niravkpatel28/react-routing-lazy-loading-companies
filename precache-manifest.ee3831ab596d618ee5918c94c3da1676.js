c0b670840f6a52f65334a693e7ac7796f4b4a601
Nirav Patel <niravkpatel28@gmail.com>
2020-05-13 22:56:57 +0530

    Updates

---
+++ precache-manifest.ee3831ab596d618ee5918c94c3da1676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ef34def88fd03315a10d20df2ed74f",
+    "url": "/react-routing-lazy-loading-companies/index.html"
+  },
+  {
+    "revision": "deb94ea2d0deba148a42",
+    "url": "/react-routing-lazy-loading-companies/static/css/main.f024190e.chunk.css"
+  },
+  {
+    "revision": "8442a7fc284009ec6b51",
+    "url": "/react-routing-lazy-loading-companies/static/js/2.d9112b8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-routing-lazy-loading-companies/static/js/2.d9112b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb94ea2d0deba148a42",
+    "url": "/react-routing-lazy-loading-companies/static/js/main.ffbd8d7b.chunk.js"
+  },
+  {
+    "revision": "458aabf2133997a6fd24",
+    "url": "/react-routing-lazy-loading-companies/static/js/runtime-main.8724cd1b.js"
+  }
+]);
