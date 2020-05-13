6aa8f4d6230d77e901b3795333eeacd2d6be4c3e
Nirav Patel <niravkpatel28@gmail.com>
2020-05-13 22:59:22 +0530

    GitHub pages added

--- src/routes/router.js
+++ src/routes/router.js
@@ -10,7 +10,7 @@ const Router = (props) => {
   //create routes for the app
   console.log(props);
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/raw" component={Raw} />
